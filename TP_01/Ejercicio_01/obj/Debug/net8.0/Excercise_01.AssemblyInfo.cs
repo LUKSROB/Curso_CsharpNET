17d87105a0b401cbb286f33352729501554bd8bb
Luksrob <lukasrob025@gmail.com>
2024-05-21 01:13:57 -0300

    .

--- TP_01/Ejercicio_01/obj/Debug/net8.0/Excercise_01.AssemblyInfo.cs
+++ TP_01/Ejercicio_01/obj/Debug/net8.0/Excercise_01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Excercise_01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bd61731fd9187afb1021c6478af38c396f9e234")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7218ff47e020f04128b7437fb9bc5dc3885bae5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Excercise_01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Excercise_01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TP_01/Ejercicio_05/obj/Debug/net8.0/Ejercicio_05.AssemblyInfo.cs
+++ TP_01/Ejercicio_05/obj/Debug/net8.0/Ejercicio_05.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio_05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bd61731fd9187afb1021c6478af38c396f9e234")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7218ff47e020f04128b7437fb9bc5dc3885bae5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejercicio_05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio_05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
