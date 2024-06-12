27ecb2da401702476a2555a81dd3794128665883
Luksrob <lukasrob025@gmail.com>
2024-06-11 23:33:47 -0300

    add

--- TP_01/Ejercicio_01/obj/Debug/net8.0/Excercise_01.AssemblyInfo.cs
+++ TP_01/Ejercicio_01/obj/Debug/net8.0/Excercise_01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Excercise_01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0027c8044bdfbd737a3482d3b150d4b19cb390b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f57c5abf0ffd37f725a66af06026f167483b181")]
 [assembly: System.Reflection.AssemblyProductAttribute("Excercise_01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Excercise_01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
