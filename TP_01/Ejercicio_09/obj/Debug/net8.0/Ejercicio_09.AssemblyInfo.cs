caf9365534e809c178f84ca606b2da2d75fe0792
Luksrob <lukasrob025@gmail.com>
2024-08-23 15:30:46 -0300

    .

--- TP_01/Ejercicio_09/obj/Debug/net8.0/Ejercicio_09.AssemblyInfo.cs
+++ TP_01/Ejercicio_09/obj/Debug/net8.0/Ejercicio_09.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio_09")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6575c758aced130b6ca2fd6b9255b81d2f55dfe8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80dd2b26f0075143507f769186029077353acc36")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejercicio_09")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio_09")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
